32682f41662fb848bb44a2d75e1cf2f218b7e807
kimiiz55 <pongnares.ct@gmail.com>
2020-12-11 15:43:16 +0700

    placeholder

--- apps/web/components/form-modal.tsx
+++ apps/web/components/form-modal.tsx
@@ -104,7 +104,7 @@ const FormModal = () => {
                 name="title"
                 ref={register({ required: true })}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder="john@example.com"
+                placeholder="Name of the task"
               />
               {errors.title && (
                 <span className="text-base text-red-400" id="reqired-title">
